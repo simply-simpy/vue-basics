b339f90b3cdfa4c82e9c58a5c37ae010400f8595
Scott Simpson <Scott@simply-simpy.com>
2021-03-17 10:51:55 -0700

    basic setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue-cli-project
+# my-vue-cli
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <Buster msg="pup" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import Buster from '@/components/Buster';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Buster
+    HelloWorld
   }
 }
 </script>
